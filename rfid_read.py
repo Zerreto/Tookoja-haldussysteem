4b4f712b158324f7764344259952ae5be30306bb
Zerreto <59620824+Zerreto@users.noreply.github.com>
2025-12-14 21:54:27 +0200

    RFID try 7

--- rfid_read.py
+++ rfid_read.py
@@ -2,45 +2,56 @@ from periphery import GPIO
 import spidev
 from time import sleep
 
-# Setup RST
-rst = GPIO(25, "out")
+# Use GPIO chip and line for RST (line 25 = BCM25)
+rst = GPIO("/dev/gpiochip0", 25, "out")
 rst.write(True)
 
-# Setup SPI
+# SPI setup
 spi = spidev.SpiDev()
 spi.open(0, 0)
-spi.max_speed_hz = 500000
+spi.max_speed_hz = 500000  # safe for RC522
 
-# Registers
-FIFO = 0x09
-COMMAND = 0x01
-BIT_FRAMING = 0x0D
-REQIDL = 0x26
-ANTICOLL = 0x93
+# RC522 registers
+FIFO, COMMAND, BIT_FRAMING, ERROR_REG = 0x09, 0x01, 0x0D, 0x06
+REQIDL, ANTICOLL = 0x26, 0x93
 
-def write_reg(a, v): spi.xfer([(a<<1)&0x7E, v])
-def read_reg(a): return spi.xfer([((a<<1)&0x7E)|0x80,0])[1]
+def write_reg(addr, val):
+    spi.xfer([(addr << 1) & 0x7E, val])
 
-# Simple init
+def read_reg(addr):
+    return spi.xfer([((addr << 1) & 0x7E) | 0x80, 0])[1]
+
+# Reset RC522
 write_reg(COMMAND, 0x0F)
 sleep(0.05)
 
-print("Hold a card near the reader...")
+last_uid = None
+print("Hold card near reader...")
 
-while True:
-    write_reg(BIT_FRAMING, 0x07)
-    write_reg(FIFO, REQIDL)
-    write_reg(COMMAND, 0x0C)
-    sleep(0.05)
-    if read_reg(0x06)==0:  # ERROR_REG == 0 means card detected
-        write_reg(BIT_FRAMING, 0x00)
-        write_reg(FIFO, ANTICOLL)
+try:
+    while True:
+        # Request card
+        write_reg(BIT_FRAMING, 0x07)
+        write_reg(FIFO, REQIDL)
         write_reg(COMMAND, 0x0C)
-        sleep(0.05)
-        uid = [read_reg(FIFO) for _ in range(5)]
-        print("Card UID:", uid)
-        break
-    sleep(0.2)
+        sleep(0.02)
 
-spi.close()
-rst.close()
+        if read_reg(ERROR_REG) == 0:
+            # Card detected, read UID
+            write_reg(BIT_FRAMING, 0x00)
+            write_reg(FIFO, ANTICOLL)
+            write_reg(COMMAND, 0x0C)
+            sleep(0.02)
+            uid = [read_reg(FIFO) for _ in range(5)]
+
+            if uid != last_uid:
+                print("Card UID:", uid)
+                last_uid = uid
+        else:
+            last_uid = None
+
+        sleep(0.02)  # ~50 reads/sec
+except KeyboardInterrupt:
+    spi.close()
+    rst.close()
+    print("Exit.")

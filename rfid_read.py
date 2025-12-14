e372b88564ad279aeecad025a4c11a7987f928db
Zerreto <59620824+Zerreto@users.noreply.github.com>
2025-12-14 19:52:08 +0200

    ei jaksa

--- rfid_read.py
+++ rfid_read.py
@@ -1,44 +1,23 @@
-"""
-This Raspberry Pi code was developed by newbiely.com
-This Raspberry Pi code is made available for public use without any restriction
-For comprehensive instructions and wiring diagrams, please visit:
-https://newbiely.com/tutorials/raspberry-pi/raspberry-pi-rfid-door-lock
-"""
-
-
-import RPi.GPIO as GPIO
-import MFRC522
+from pirc522 import RFID
 import time
 
-# Define GPIO pins
-RC522_RST_PIN = 12  # GPIO pin connected to RC522's RST pin
-RELAY_PIN = 16      # GPIO pin connected to relay
+rdr = RFID(bus=0, device=0)
 
-# Set up GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(RELAY_PIN, GPIO.OUT, initial=GPIO.LOW)
-
-# Create an instance of the RFID reader
-reader = MFRC522.MFRC522()
-
-authorized_uid = [0xFF, 0xFF, 0xFF, 0xFF]
+print("RC522 ready (Raspberry Pi 5)")
+print("Scan a card...")
 
 try:
-    print("Tap RFID/NFC Tag on reader")
     while True:
-        (status, TagType) = reader.MFRC522_Request(reader.PICC_REQIDL)
-
-        if status == reader.MI_OK:
-            (status, uid) = reader.MFRC522_Anticoll()
-
-            if status == reader.MI_OK:
-                if uid == authorized_uid:
-                    print("Access is granted")
-                    GPIO.output(RELAY_PIN, GPIO.HIGH)  # unlock the door for 2 seconds
-                    time.sleep(2)
-                    GPIO.output(RELAY_PIN, GPIO.LOW)   # lock the door
-                else:
-                    print(f"Access denied for user with UID: {' '.join(format(b, '02x') for b in uid)}")
+        rdr.wait_for_tag()
+        (err, _) = rdr.request()
+        if not err:
+            (err, uid) = rdr.anticoll()
+            if not err:
+                print("Card UID:", uid)
+                time.sleep(1)
 
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    print("\nExiting")
+
+finally:
+    rdr.cleanup()

a7bdc2f9a26c77b6ddc44dfc8c61709e68ff8f90
Zerreto <59620824+Zerreto@users.noreply.github.com>
2025-12-14 21:43:58 +0200

    RFID try 6

--- rfid_read.py
+++ rfid_read.py
@@ -1,23 +1,46 @@
-from pirc522 import RFID
-import time
+from periphery import GPIO
+import spidev
+from time import sleep
 
-rdr = RFID(bus=0, device=0)
+# Setup RST
+rst = GPIO(25, "out")
+rst.write(True)
 
-print("RC522 ready (Raspberry Pi 5)")
-print("Scan a card...")
+# Setup SPI
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 500000
 
-try:
-    while True:
-        rdr.wait_for_tag()
-        (err, _) = rdr.request()
-        if not err:
-            (err, uid) = rdr.anticoll()
-            if not err:
-                print("Card UID:", uid)
-                time.sleep(1)
+# Registers
+FIFO = 0x09
+COMMAND = 0x01
+BIT_FRAMING = 0x0D
+REQIDL = 0x26
+ANTICOLL = 0x93
 
-except KeyboardInterrupt:
-    print("\nExiting")
+def write_reg(a, v): spi.xfer([(a<<1)&0x7E, v])
+def read_reg(a): return spi.xfer([((a<<1)&0x7E)|0x80,0])[1]
 
-finally:
-    rdr.cleanup()
+# Simple init
+write_reg(COMMAND, 0x0F)
+sleep(0.05)
+
+print("Hold a card near the reader...")
+
+while True:
+    write_reg(BIT_FRAMING, 0x07)
+    write_reg(FIFO, REQIDL)
+    write_reg(COMMAND, 0x0C)
+    sleep(0.05)
+    if read_reg(0x06)==0:  # ERROR_REG == 0 means card detected
+        write_reg(BIT_FRAMING, 0x00)
+        write_reg(FIFO, ANTICOLL)
+        write_reg(COMMAND, 0x0C)
+        sleep(0.05)
+        uid = [read_reg(FIFO) for _ in range(5)]
+        print("Card UID:", uid)
+        break
+    sleep(0.2)
+
+spi.close()
+rst.close()
